33b8b76fd65bcf7788a23d73623889ca698a06d1
gogo <gogo@musiq.ch>
2017-11-22 14:10:10 +0100

    migrations

--- app/DoctrineMigrations/Version20160412165619.php
+++ app/DoctrineMigrations/Version20160412165619.php
@@ -8,7 +8,7 @@ use Doctrine\DBAL\Schema\Schema;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-class Version20171121133522 extends AbstractMigration
+class Version20160412165619 extends AbstractMigration
 {
     /**
      * @param Schema $schema
@@ -16,9 +16,9 @@ class Version20171121133522 extends AbstractMigration
     public function up(Schema $schema)
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('ALTER TABLE user ADD password VARCHAR(255) NOT NULL');
+        $this->addSql('ALTER TABLE genus ADD first_discovered_at DATE NOT NULL');
     }
 
     /**
@@ -27,8 +27,8 @@ class Version20171121133522 extends AbstractMigration
     public function down(Schema $schema)
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('ALTER TABLE user DROP password');
+        $this->addSql('ALTER TABLE genus DROP first_discovered_at');
     }
 }
--- app/DoctrineMigrations/Version20160524141318.php
+++ app/DoctrineMigrations/Version20160524141318.php
@@ -8,7 +8,7 @@ use Doctrine\DBAL\Schema\Schema;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-class Version20171112171346 extends AbstractMigration
+class Version20160524141318 extends AbstractMigration
 {
     /**
      * @param Schema $schema
@@ -16,9 +16,9 @@ class Version20171112171346 extends AbstractMigration
     public function up(Schema $schema)
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('ALTER TABLE genus ADD is_published TINYINT(1) NOT NULL');
+        $this->addSql('ALTER TABLE user ADD roles LONGTEXT NOT NULL COMMENT \'(DC2Type:json_array)\'');
     }
 
     /**
@@ -27,8 +27,8 @@ class Version20171112171346 extends AbstractMigration
     public function down(Schema $schema)
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('ALTER TABLE genus DROP is_published');
+        $this->addSql('ALTER TABLE user DROP roles');
     }
 }
